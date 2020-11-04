b671881f8381352d0558fb9a97fad2187c6281cd
TaehaKim <taeha7b@gmail.com>
2020-11-04 23:34:52 +0900

    Add: 게시판 모델 작성

---
+++ post/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Post(models.Model):
+    user       = models.ForeignKey('user.User', on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    content    = models.CharField(max_length = 1024)
+    deleted_at = models.DateTimeField(null = True, blank = True)
+    is_deleted = models.BooleanField(default = False)
+
+    class Meta:
+        db_table = 'posts'
